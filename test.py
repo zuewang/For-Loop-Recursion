464980006249f30b74ce3cc0f392697c71f02a1d
Aron <240501541@qq.com>
2016-09-09 00:26:54 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,3 +6,17 @@ def fr(set, n):
 			set.append(n*100+i)
 			fr(set, n-1)
 fr([], 3)
+
+def fr2(set, n):
+	if n == 1:
+		print set
+	else:
+		for i in range(0, 3):
+			#reset set
+			while len(set) > 3 - n:
+				set.pop()
+			
+			set.append(n*100+i)
+			fr2(set, n-1)
+
+fr2([], 3)
